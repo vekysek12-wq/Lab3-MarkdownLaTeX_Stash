8d31c68ba7ef106a7692dd48dc69c769f744cdb2
vekys <vekysek12@gmail.com>
2026-09-20 22:30:53 +0300

    Added text formatting examples and explanations

---
+++ FormatDemoApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FormatDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число: ");
+            double number1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число: ");
+            double number2 = Convert.ToDouble(Console.ReadLine());
+
+            double sum = number1 + number2;
+
+            Console.WriteLine($"**Результаты операций: {sum}**");
+        }
+    }
+}
